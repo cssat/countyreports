043b637a2b909d9afae5ad5816e415eddc41ee63
Gregor Thomas <gregor.passolt@gmail.com>
2015-09-01 10:12:31 -0700

    updates working for King, now try all

--- reports/county_reports.R
+++ reports/county_reports.R
@@ -4,83 +4,88 @@
 ## @knitr frontmatter
 library(pocr)
 library(dplyr)
+library(RODBC)
+library(stringr)
+library(lubridate)
+library(xtable)
+library(extrafont)
 
 # display_county <- "King"
 
 
 ## Updated version KEEP ME!
-
-
-trend_plot3 <-
- function (trend_data, type = c("ooh", "ia", "ihs"), title = NA, 
-    title_size = 1.2, font = "Frutiger LT Std 45 Light") {
-    type <- match.arg(type)
-    names(trend_data) <- c("date", "count", "geo")
-    switch(type, ooh = {
-        stock_ylab <- paste0("Total Cases First Day")
-        if (is.na(title)) title <- "Trends in Out-of-Home Care"
-        levels(trend_data$geo) <- paste(levels(trend_data$geo), 
-            "County")
-    }, ia = {
-        stock_ylab <- paste0("Total Cases First Day")
-        if (is.na(title)) title <- "Trends in Investigations & Assessments"
-        levels(trend_data$geo) <- paste(levels(trend_data$geo), 
-            "DCFS Office")
-    }, ihs = {
-        stock_ylab <- paste0("Total Cases First Day")
-        if (is.na(title)) title <- "Trends in In-Home Service"
-        levels(trend_data$geo) <- paste(levels(trend_data$geo), 
-            "DCFS Office")
-    })
-    trend_data <- ddply(trend_data, .variables = "geo", mutate, 
-        text_pos = ifelse(count > 0, count - 0.05 * max(count), 
-            0.05 * max(count)), gtzero = factor(count > 0, levels = c(TRUE, 
-            FALSE)))
-    tp <- ggplot(trend_data, aes(x = date, y = count)) + 
-        facet_wrap(~geo, ncol = 1, scales = "free") + 
-        geom_bar(stat = "identity", fill = poc_colors[1], color = NA) + 
-        geom_text(aes(label = formatC(count, big.mark = ","), y = text_pos, color = gtzero), size = 2.5, family = font) + 
-        scale_colour_manual(values = c("white", "black")) + 
-        theme_bw() + scale_y_continuous(labels = comma_format()) + 
-        labs(x = "", y = stock_ylab, title = title) + 
-        theme(text = element_text(family = font), 
-              axis.title.y = element_text(vjust = .5), 
-              plot.margin = unit(c(1, 1, 1, 1), "lines"), 
-              plot.title = element_text(size = rel(title_size), vjust = 1, hjust = 0), 
-              strip.background = element_rect(fill = poc_colors[3], size = NA), legend.position = "none")
-    if (length(levels(trend_data$geo)) == 1) {
-        tp <- tp + scale_y_continuous(limits = c(0, 1.1 * max(trend_data$count)))
-    }
-    print(tp)
-}
-
-context_plot <-
-function (context_data, focus = "none", xlab, title = "", state_label = "Washington", 
-    colors = portal_colors[c(8, 4, 2)], title_size = 1.2, font = "Frutiger LT Std 45 Light") {
-    names(context_data)[1:2] <- c("focus_group", "x_data")
-    context_data$focus_indicator <- ifelse(context_data[, 1] %in% 
-        focus, 1, ifelse(context_data[, 1] %in% state_label, 
-        2, 0))
-    context_data$focus_indicator <- factor(context_data$focus_indicator)
-    context_data[, 1] <- as.character(context_data[, 1])
-    too_long <- nchar(context_data[, 1]) > 34
-    context_data[too_long, 1] <- str_replace(context_data[too_long, 
-        1], pattern = fixed("("), replacement = "\n(")
-    context_data[, 1] <- factor(context_data[, 1])
-    context_data[, 1] <- reorder(x = context_data[, 1], X = context_data[, 
-        2], order = TRUE)
-    ggplot(context_data, aes_string(x = "x_data", y = "focus_group", 
-        color = "focus_indicator")) + geom_point(size = 5, shape = 18) + 
-        labs(x = xlab, y = "", title = title) + theme_bw() + 
-        scale_x_continuous(limits = c(0, 1.15 * max(context_data$x_data)), 
-            expand = c(0, 0)) + scale_colour_manual(values = colors) + 
-        theme(legend.position = "none", text = element_text(family = font), 
-            axis.title = element_text(size = rel(0.8)), axis.text.y = element_text(size = rel(0.8), 
-                vjust = 0.5), plot.title = element_text(size = rel(title_size), 
-                hjust = 0), plot.margin = unit(c(1, 1, 1, 1), 
-                "lines"))
-}
-
+# 
+# 
+# trend_plot3 <-
+#  function (trend_data, type = c("ooh", "ia", "ihs"), title = NA, 
+#     title_size = 1.2, font = "Frutiger LT Std 45 Light") {
+#     type <- match.arg(type)
+#     names(trend_data) <- c("date", "count", "geo")
+#     switch(type, ooh = {
+#         stock_ylab <- paste0("Total Cases First Day")
+#         if (is.na(title)) title <- "Trends in Out-of-Home Care"
+#         levels(trend_data$geo) <- paste(levels(trend_data$geo), 
+#             "County")
+#     }, ia = {
+#         stock_ylab <- paste0("Total Cases First Day")
+#         if (is.na(title)) title <- "Trends in Investigations & Assessments"
+#         levels(trend_data$geo) <- paste(levels(trend_data$geo), 
+#             "DCFS Office")
+#     }, ihs = {
+#         stock_ylab <- paste0("Total Cases First Day")
+#         if (is.na(title)) title <- "Trends in In-Home Service"
+#         levels(trend_data$geo) <- paste(levels(trend_data$geo), 
+#             "DCFS Office")
+#     })
+#     trend_data <- ddply(trend_data, .variables = "geo", mutate, 
+#         text_pos = ifelse(count > 0, count - 0.05 * max(count), 
+#             0.05 * max(count)), gtzero = factor(count > 0, levels = c(TRUE, 
+#             FALSE)))
+#     tp <- ggplot(trend_data, aes(x = date, y = count)) + 
+#         facet_wrap(~geo, ncol = 1, scales = "free") + 
+#         geom_bar(stat = "identity", fill = poc_colors[1], color = NA) + 
+#         geom_text(aes(label = formatC(count, big.mark = ","), y = text_pos, color = gtzero), size = 2.5, family = font) + 
+#         scale_colour_manual(values = c("white", "black")) + 
+#         theme_bw() + scale_y_continuous(labels = comma_format()) + 
+#         labs(x = "", y = stock_ylab, title = title) + 
+#         theme(text = element_text(family = font), 
+#               axis.title.y = element_text(vjust = .5), 
+#               plot.margin = unit(c(1, 1, 1, 1), "lines"), 
+#               plot.title = element_text(size = rel(title_size), vjust = 1, hjust = 0), 
+#               strip.background = element_rect(fill = poc_colors[3], size = NA), legend.position = "none")
+#     if (length(levels(trend_data$geo)) == 1) {
+#         tp <- tp + scale_y_continuous(limits = c(0, 1.1 * max(trend_data$count)))
+#     }
+#     print(tp)
+# }
+# 
+# context_plot <-
+# function (context_data, focus = "none", xlab, title = "", state_label = "Washington", 
+#     colors = portal_colors[c(8, 4, 2)], title_size = 1.2, font = "Frutiger LT Std 45 Light") {
+#     names(context_data)[1:2] <- c("focus_group", "x_data")
+#     context_data$focus_indicator <- ifelse(context_data[, 1] %in% 
+#         focus, 1, ifelse(context_data[, 1] %in% state_label, 
+#         2, 0))
+#     context_data$focus_indicator <- factor(context_data$focus_indicator)
+#     context_data[, 1] <- as.character(context_data[, 1])
+#     too_long <- nchar(context_data[, 1]) > 34
+#     context_data[too_long, 1] <- str_replace(context_data[too_long, 
+#         1], pattern = fixed("("), replacement = "\n(")
+#     context_data[, 1] <- factor(context_data[, 1])
+#     context_data[, 1] <- reorder(x = context_data[, 1], X = context_data[, 
+#         2], order = TRUE)
+#     ggplot(context_data, aes_string(x = "x_data", y = "focus_group", 
+#         color = "focus_indicator")) + geom_point(size = 5, shape = 18) + 
+#         labs(x = xlab, y = "", title = title) + theme_bw() + 
+#         scale_x_continuous(limits = c(0, 1.15 * max(context_data$x_data)), 
+#             expand = c(0, 0)) + scale_colour_manual(values = colors) + 
+#         theme(legend.position = "none", text = element_text(family = font), 
+#             axis.title = element_text(size = rel(0.8)), axis.text.y = element_text(size = rel(0.8), 
+#                 vjust = 0.5), plot.title = element_text(size = rel(title_size), 
+#                 hjust = 0), plot.margin = unit(c(1, 1, 1, 1), 
+#                 "lines"))
+# }
+# 
 
 
 #### Pick a database ####
@@ -152,18 +157,15 @@ context_fig_b <- 1.4
 
 
 ## All subsequent chunks should be able to depend on this one alone. ----
-library(xtable)
-library(extrafont)
 
 ## xtable and ggplot options
 options(xtable.NA.string = "NA")
 dotplot_colors <- portal_colors[c(8, 4, 2)]
 plot_title_size = 1.0 ## rel size
-font = "Frutiger LT Std 45 Light"
+font = "Open Sans"
 
 focus_county <- tolower(display_county)
 focus_county_cd <- ref_lookup_county$county_cd[tolower(ref_lookup_county$county) == focus_county]
-# focus_info <- county_to_office(focus_county)
 state_label <- "Washington"
 omit_ooh_counties <- c("Adams", "Asotin", "Columbia", "Ferry", "Garfield",
                        "Klickitat", "Lincoln", "Pacific", "Pend Oreille",
@@ -186,12 +188,10 @@ focus_pop_house <- sqlQuery(con, paste0("call sp_population_household(", focus_c
 #### Get data
 
 #### Focus Data ####
-ia_call  <- sp_cr("ia_trends_counts", county = focus_county)
-ihs_call <- sp_cr("ihs_trends_counts", county = focus_county)							
-ooh_call <- sp_cr("ooh_pit_counts", county = c(focus_county))
+ia_call  <- stored_procedure("ia_trends_counts", county = focus_county)
+ooh_call <- stored_procedure("ooh_pit_counts", county = c(focus_county))
 
 ia_focus  <- sqlQuery(con, ia_call)
-ihs_focus <- sqlQuery(con, ihs_call)
 ooh_focus <- sqlQuery(con, ooh_call)
 
 #### Expand focus data (filling in 0's)
@@ -199,24 +199,18 @@ ooh_focus <- sqlQuery(con, ooh_call)
 ia_focus <- cr_clean(ia_focus)
 ia_focus <- filter(ia_focus, date >= ooh_start_date)# & date <= ooh_end_date)
 
-ihs_focus <- cr_clean(ihs_focus)
-ihs_focus <- filter(ihs_focus, date >= ooh_start_date)# & date <= ooh_end_date)
-
 ooh_focus <- cr_clean(ooh_focus)
 ooh_focus <- filter(ooh_focus, date >= ooh_start_date)# & date <= ooh_end_date)
 
 #### Context Data ####
 
-ia_region_call  <- sp_cr("ia_trends_rates", county = c(0, region_counties))
-ihs_region_call <- sp_cr("ihs_trends_rates", county = c(0, region_counties))					
-ooh_region_call <- sp_cr("ooh_pit_rates", county = c(0, region_counties))
+ia_region_call  <- stored_procedure("ia_trends_rates", county = c(0, region_counties))
+ooh_region_call <- stored_procedure("ooh_pit_rates", county = c(0, region_counties))
 
 ia_region  <- sqlQuery(con, ia_region_call)
-ihs_region <- sqlQuery(con, ihs_region_call)
 ooh_region <- sqlQuery(con, ooh_region_call)
 
 ia_region  <- cr_clean(ia_region)
-ihs_region <- cr_clean(ihs_region)
 ooh_region <- cr_clean(ooh_region)
 
 ia_region_a <- filter(ia_region, date == max(date)) %>%
@@ -231,10 +225,10 @@ ia_context_date_string <- paste0("Quarter ", quarter(ia_context_date), ", ", yea
 ## Permanency Data  -- uses cohort_date
 
 perm_call <- list()
-perm_call[1] <- sp_cr("ooh_outcomes_24m", county = c("All", focus_county))
-perm_call[2] <- sp_cr("ooh_outcomes_24m", county = region_counties)
+perm_call[1] <- stored_procedure("ooh_outcomes_24m", county = c("All", focus_county))
+perm_call[2] <- stored_procedure("ooh_outcomes_24m", county = region_counties)
 
-ent_query <- sp_cr("ooh_flow_entries_counts", county = region_counties)
+ent_query <- stored_procedure("ooh_flow_entries_counts", county = region_counties)
 ent <- sqlQuery(con, ent_query)
 ent <- cr_clean(ent, date.type = 2) 
 
@@ -253,11 +247,9 @@ perm_ent <- perm2 %>% left_join(ent) %>% filter(cohort.period == cohort_period) 
     select(geo, discharge, percent)
 
 
-	
 f_county_perm <- perm1 %>% filter(cohort.period == cohort_period) %>%
 			select(county, discharge, percent) 			
 			
-
 names(f_county_perm) <- names(perm_ent)
 
 perm <- rbind(f_county_perm, perm_ent)
@@ -269,22 +261,21 @@ perm$geo <- gsub("All", "Washington", perm$geo)
 
 ## OOH Safety 
 
-ooh_safety_call <- sp_cr("ooh_reentry", date = cohort_date, county = c(0, region_counties))
+ooh_safety_call <- stored_procedure("ooh_reentry", date = cohort_date, county = c(0, region_counties))
 ooh_safety_data <- sqlQuery(con, ooh_safety_call)
 ooh_safety_data <- cr_clean(ooh_safety_data)
 
 ## Well-Being Data
-					 
-county_arg <- 0:39
+
 wb_year = last_complete$year
 
-wb_call <- sp_cr("ooh_wb_familysettings", county = county_arg)
+wb_call <- stored_procedure("ooh_wb_familysettings", county = c(0, region_counties))
 wb <- sqlQuery(con, wb_call)
 wb <- cr_clean(wb) %>%
     filter(year(date) == wb_year)
 wb <- wb[, c("date", "county", "family.setting..kin.placement.")]
 
-wb_pop_call <- sp_cr("ooh_pit_counts", county = county_arg)
+wb_pop_call <- stored_procedure("ooh_pit_counts", county = c(0, region_counties))
 wb_pop <- sqlQuery(con, wb_pop_call)
 wb_pop <- cr_clean(wb_pop, date.type = 2)
 
@@ -335,17 +326,13 @@ qf_print <- qf_print[-c(13, 15, 17), ]
 
 hl_date <- as.POSIXlt(context_date)
 
-hl_ooh <- sp_cr("ooh_pit_counts", date = context_date, county = focus_county_cd, age=0:1)
+hl_ooh <- stored_procedure("ooh_pit_counts", date = context_date, county = focus_county_cd, age=0:1)
 hl_ooh <- sqlQuery(con, hl_ooh)
 hl_ooh <- cr_clean(hl_ooh)
 
-hl_ia <- sp_cr("ia_trends_counts", date = context_date, county = focus_county_cd, age=0:1)
+hl_ia <- stored_procedure("ia_trends_counts", date = context_date, county = focus_county_cd, age=0:1)
 hl_ia <- sqlQuery(con, hl_ia)
 hl_ia <- cr_clean(hl_ia)
-
-hl_ihs <- sp_cr("ihs_trends_counts", date = context_date, county = focus_county_cd, age=0:1)
-hl_ihs <- sqlQuery(con, hl_ihs)
-hl_ihs <- cr_clean(hl_ihs)
 
 odbcCloseAll()
 
@@ -372,26 +359,24 @@ hl <- qf_print[c(1, 3, 4), 1]
 hl <- c("",
         hl,
         "",
-        formatC(hl_ia_a[1,4], big.mark=","),
+        formatC(hl_ia_a[1,4], big.mark = ","),
         # formatC(hl_ihs_a[1,6], big.mark=","),
         formatC(hl_ooh_a[hl_ooh_a$age.grouping.cd == 0, 4], big.mark = ","),
         paste0(round(100*hl_ooh_a[hl_ooh_a$age.grouping.cd == 1, 4] / hl_ooh_a[hl_ooh_a$age.grouping.cd == 0, 4]), "%"))
-hl <- str_replace_all(hl, pattern="%", replacement="\\\\%")
+hl <- stringr::str_replace_all(hl, pattern = "%", replacement = "\\\\%")
 
 hl_names <- c("\\textbf{U.S. Census Bureau (2012)}", 
               "\\quad Total Population",
               "\\quad Percent of Population Under 5 Years",
               "\\quad Percent of Population Under 18 Years",
-              #paste0("\\textbf{Child Well-Being Data Portal (", pretty_date(hl_date), ")}"),
               paste0("\\textbf{Child Well-Being Data Portal}"),
               paste0("\\quad Number of Open Investigations \\& Assessments, (", pretty_date(hl_ia_a$date[[1]]), ")"),
-              #"\\quad Number of Open Placement Prevention Services Cases",
               paste0("\\quad Number of Open Out-of-Home Care Cases, (", pretty_date(hl_ooh_a$date[[1]]), ")"),
               paste0("\\quad Percent of Out-of-Home Care Cases: Children Under 5 Years, (", pretty_date(hl_ooh_a$date[[1]]), ")"))
 
 hl <- data.frame(v1 = hl_names, v2 = hl)
 
-print.xtable(xtable(hl, align="llr"),
+print.xtable(xtable(hl, align = "llr"),
              sanitize.text.function = as.character,
              size = "\\small",
              include.rownames = FALSE, 
@@ -404,7 +389,7 @@ print.xtable(xtable(hl, align="llr"),
 ## @knitr ia_focus
 ia_focus_a <- ia_focus[,c(1:2)]
 ia_focus_a$county <- paste(display_county, "County")
-trend_plot3(ia_focus_a, type = "ia",
+trend_plot(ia_focus_a, type = "ia",
             title_size = plot_title_size)
 
 
@@ -425,14 +410,14 @@ context_plot(ia_region_a,
 ooh_focus_a <- ooh_focus
 ooh_focus_a$geo = paste(display_county, "County")
 
-trend_plot3(ooh_focus_a, type = "ooh")
+trend_plot(ooh_focus_a, type = "ooh")
 
 ## ooh_context ----
 ## @knitr ooh_context
 
-ooh_region_a <- dplyr::filter(ooh_region, date == context_date)			 
-ooh_region_a <- dplyr::select(ooh_region_a, county, total.in.out.of.home.care.1st.day)
-ooh_region_a$county <- gsub("All", "Washington", ooh_region_a$county)
+ooh_region_a <- dplyr::filter(ooh_region, date == context_date)	%>%
+    select(county, total.in.out.of.home.care.1st.day) %>%
+    mutate(county = str_replace(county, "All", "Washington"))
 
 ooh_region_a <- ooh_region_a[ooh_region_a$county %nin% omit_ooh_counties, , drop = TRUE]
 
@@ -460,7 +445,6 @@ ooh_safety_data$county[ooh_safety_data$county == state_label] <- paste0("\\textb
 
 ooh_safety_data[, 2] <- paste0(round(ooh_safety_data[, 2]), "%")
 
-
 names(ooh_safety_data) <-  c("County", " Re-Entry")
 
 ooh_safety_cap <- paste('Percentage of Children Re-Entering Out-of-Home Care within One Year of Exiting Out-of-Home Care,',
@@ -486,7 +470,7 @@ ggplot(perm, aes(x = discharge, y = percent/100, fill = geo)) +
     theme_bw() +
     geom_bar(position = "dodge", stat = "identity") +
     scale_fill_manual(values = dotplot_colors) +
-    scale_y_continuous(labels = percent) +
+    scale_y_continuous(labels = scales::percent) +
     labs(x = "",
          y = "Percent experiencing outcome",
          fill = "",
@@ -515,7 +499,7 @@ ggplot(wb_context, aes(x = family.setting..kin.placement. / 100,
     labs(x = "Percent of Children in Kinship Care",
          y = "",
          title = wb_title) +
-    scale_x_continuous(labels = percent_format(),
+    scale_x_continuous(labels = scales::percent,
                        limits = c(.00, 1.15 * max(wb_context$family.setting..kin.placement. / 100)),
                        expand = c(0, 0)) +
     theme_bw() +
--- reports/template_write_reports.R
+++ reports/template_write_reports.R
@@ -2,6 +2,7 @@
 
 ## Set working directory appropriately!
 #setwd("S:/Data Portal/county_reports/reports")
+# Use an RProject!
 
 ## Remember to
 ##  - disable focus_county in county_reports.R
@@ -13,9 +14,11 @@ require(brew)
 require(knitr)
 require(RCurl)
 require(tools)
+library(stringr)
+library(lubridate)
 setwd("reports/")
 
-write_report <- function(county_arg, upload = FALSE){
+write_report <- function(county_arg){
     Sys.setenv(PDFLATEX = "xelatex")
     display_county <<- county_arg
     rnw_file <- sprintf("county_report_%s.Rnw", county_arg)
@@ -25,12 +28,6 @@ write_report <- function(county_arg, upload = FALSE){
              compiler = "xelatex")
     texi2pdf(file = str_replace(rnw_file, pattern = "Rnw", replacement = "tex"),
              clean = TRUE)
-    if (upload) {
-        pdf_file <- sprintf("county_report_%s.pdf", county_arg)
-        ftpUpload(what = pdf_file,
-                  to = paste0("sftp://gregorp:PASSWORD@pocweb.cac.washington.edu/data/pocweb/www-review/county_reports/",
-                              pdf_file))
-    }
 }
 
 # produce reports
@@ -39,12 +36,11 @@ omit_ooh_counties <- c("Adams", "Asotin", "Columbia", "Ferry", "Garfield",
                        "San Juan", "Skamania", "Wahkiakum")
 counties <- ref_lookup_county[1:39, "county"]
 counties <- counties[counties %nin% omit_ooh_counties]
-#counties <- "King"
+counties <- "King"
 #counties <- c("Cowlitz", "Clark", "Clallam", "Jefferson", "Thurston")
 #counties <- c("Grays Harbor", "San Juan", "Pend Oreille", "Walla Walla")
 #counties <- c("Benton", "Franklin", "Kittitas", "Klickitat", "Walla Walla", "Yakima")
 
-#plyr::l_ply(counties, function(x) write_report(x, upload = FALSE))
 
 for (i in seq_along(counties)) {
     try(write_report(counties[i]))
